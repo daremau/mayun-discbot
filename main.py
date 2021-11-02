a9dfd47045b5642163aa0a80198de9a12ad64a67
contactlight <52682005+contactlightV@users.noreply.github.com>
2021-11-02 16:23:41 -0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.client import Client
 from discord.ext import commands
 import youtube_dl
 import os
@@ -9,51 +10,38 @@ client = commands.Bot(command_prefix="-")
 async def on_ready():
     print(f"{client.user.name} is ready.")
 
-'''@client.event
-async def on_voice_state_update(self, member, before, after):
-    
-    if not member.id == self.bot.user.id:
-        return
+@client.command()
+async def check_user_in_channel(ctx):
+    voice_state = ctx.author.voice
 
-    elif before.channel is None:
-        voice = after.channel.guild.voice_client
-        time = 0
-        while True:
-            await asyncio.sleep(1)
-            time = time + 1
-            if voice.is_playing() and not voice.is_paused():
-                time = 0
-            if time == 600:
-                await voice.disconnect()
-            if not voice.is_connected():
-                break'''
+    if voice_state is None:
+        return await ctx.send('Conectate al canal')
 
 @client.command()
 async def play(ctx, url):
-    try:
-        voice_channel = ctx.author.voice.channel
-        
-        if ctx.voice_client is None:
-            await voice_channel.connect()
-       
-        ctx.voice_client.stop()
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        YDL_OPTIONS = {'format':"bestaudio"}
-        vc = ctx.voice_client
-        
-        await ctx.send('<:Buenardo:784888763343306762>')
-        
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'] [0] ['url']
-        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        vc.play(source)
-    except:
-        print('no se encuentra ffmpeg')
+    check_user_in_channel(ctx)    
+    voice_channel = ctx.author.voice.channel
+    
+    if ctx.voice_client is None:
+        await voice_channel.connect()
 
+    ctx.voice_client.stop()
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    YDL_OPTIONS = {'format':"bestaudio"}
+    vc = ctx.voice_client
 
+    await ctx.send('<:Buenardo:784888763343306762>')
+
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        info = ydl.extract_info(url, download=False)
+        url2 = info['formats'] [0] ['url']
+    source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+    vc.play(source)
+    print(f"{ctx.author.name} esta escuchando {info['title']}")
+    
 @client.command()
 async def leave(ctx):
+    check_user_in_channel(ctx)
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_connected():
         await voice.disconnect()
@@ -61,29 +49,33 @@ async def leave(ctx):
     else:
         await ctx.send('???????')
 
-
 @client.command()
 async def pause(ctx):
+    check_user_in_channel(ctx)
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_playing():
         voice.pause()
     else:
         await ctx.send('????????')
 
-
 @client.command()
 async def resume(ctx):
+    check_user_in_channel(ctx)
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_paused():
         voice.resume()
     else:
         await ctx.send('???????')
 
-
 @client.command()
 async def stop(ctx):
+    check_user_in_channel(ctx)
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     voice.stop()
     await ctx.send('<:TAVYRON:821417147435712573>')
 
-client.run('ODg3NDU5MTU5MDUyNzgzNjY3.YUEcvw.n4Li5ppg16uU6DVLfSGjN4jOmEM')
+@client.command(name="pilin")
+async def pilin(ctx):
+    await ctx.send("pilin")
+
+client.run('OTAxOTgzNDg0MDI4MDAyMzM1.YXXzlA.PoYr8cwWqAjoNYemG2wAGktjjf0')
